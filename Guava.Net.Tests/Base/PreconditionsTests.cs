ec350e068301046fa7608bf7cc2a1cd23c97d891
DForshner <DForshner@gmail.com>
2014-05-21 11:12:37 -0400

    Added Preconditions

---
+++ Guava.Net.Tests/Base/PreconditionsTests.cs
@@ -0,0 +1,98 @@
+﻿using Guava.Net.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+/*
+ * Copyright (C) 2014 David Forshner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
+ */
+
+namespace Guava.Net.Tests.Base
+{
+    [TestClass]
+    public class PreconditionsTests
+    {
+        [TestMethod]
+        public void CheckArgument_WhenTrueNoArgs_ExpectOK()
+        {
+            Preconditions.CheckArgument(true);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckArgument_WhenFalse_ExpectException()
+        {
+            Preconditions.CheckArgument(false);
+        }
+
+        [TestMethod]
+        public void CheckArgument_WhenTrue_ExpectOK()
+        {
+            Preconditions.CheckArgument(true, IGNORE_ME);
+        }
+
+        [TestMethod]
+        //[ExpectedException(typeof(ArgumentException))]
+        public void CheckArgument_WhenTrue_ExpectSimpleExceptionMessage()
+        {
+            try
+            {
+                Preconditions.CheckArgument(false, new Message());
+            }
+            catch (Exception e)
+            {
+                verifySimpleMessage(e);
+            }
+        }
+
+        [TestMethod]
+        public void CheckArgument_WhenNullMessage_ExpectException()
+        {
+            
+        }
+
+        private class IgnoreMe : Object
+        {
+            public override string ToString()
+            {
+                throw new AssertFailedException("Expected object to be ignored.");
+            }
+        }
+        private static Object IGNORE_ME = new IgnoreMe();
+
+        private class Message
+        {
+            bool invoked;
+            public override string ToString()
+            {
+                Assert.IsFalse(invoked);
+                invoked = true;
+                return "A message";
+            }
+        }
+
+        private const String FORMAT = "I ate %s pies.";
+
+        private static void verifySimpleMessage(Exception e)
+        {
+            Assert.Equals("A message", e.Message);
+        }
+        
+        private static void VerifyComplextMessage(Exception e)
+        {
+            //Assert.Equals("I ate 5 pies.", e.getMessage());
+        }
+    }
+}
---
+++ Guava.Net/Base/Preconditions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guava.Net.Base
+{
+    /// <summary>
+    /// Static convenience methods that help a method or constructor check whether it was invoked correctly
+    /// (Whether its preconditions have been met).
+    /// </summary>
+    public static class Preconditions
+    {
+
+        /// <summary>
+        /// Ensure that an object reference passed as a parameter to the calling method is not null.
+        /// </summary>
+        public static T CheckNotNull<T>(this T reference)
+        {
+            if (reference == null)
+                throw new NullReferenceException();
+            return reference;
+        }
+
+        /// <summary>
+        /// Ensure that an object reference passed as a parameter to the calling method is not null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reference">Reference to object</param>
+        /// <param name="errorMessageTemplate">Template for the exception message should the check fail.</param>
+        /// <param name="errorMessageArgs">The arguments to be substituted into the message template.</param>
+        /// <returns>The non-null reference that was validated.</returns>
+        public static T CheckNotNull<T>(this T reference, string errorMessageTemplate, params object[] errorMessageArgs)
+        {
+            if (reference == null)
+                throw new NullReferenceException(String.Format(errorMessageTemplate, errorMessageArgs));
+            return reference;
+        }
+
+        public static void CheckArgument(bool expression)
+        {
+            if (!expression)
+                throw new ArgumentException();
+        }
+
+        /// <summary>
+        /// Ensures the true of an expression involving one or more parameters to a calling method.
+        /// </summary>
+        /// <param name="expression">Boolean expression</param>
+        /// <param name="errorMessage">Exception message to use when check fails.</param>
+        public static void CheckArgument(bool expression, object errorMessage)
+        {
+            if (!expression)
+                throw new ArgumentException(errorMessage.ToString());
+        }
+
+        public static void CheckArgument(bool expression, string errorMessageTemplate, params object[] errorMessageArgs)
+        {
+            if (!expression)
+                throw new ArgumentException();
+        }
+    }
+}
